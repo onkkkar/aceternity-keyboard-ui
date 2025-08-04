3bea8f84b4b22309f11acc85846de38aa80ed666
Onkar Patel <onkarpatel10@gmail.com>
2025-08-04 18:46:24 +0530

    feat: Added Arrow Keys Leyboard is Complete

--- src/components/row6.tsx
+++ src/components/row6.tsx
@@ -186,8 +186,131 @@ const Row6 = () => {
         </div>
       </div>
 
-
       {/* Arrow Keys */}
+      <div className="mt-[2px] flex h-6 w-[4.9rem] flex-col items-center justify-end rounded-[4px] p-[0.5px]">
+        <div className="cursor-pointer rounded-[4px] bg-white/[0.2] p-[0.5px] shadow-md shadow-white/50 transition-transform duration-100 hover:scale-[0.95] hover:shadow-none">
+          <div
+            className="flex h-3 w-6 items-center justify-center rounded-[3.5px] bg-[#0A090D]"
+            style={{
+              boxShadow:
+                "rgb(13, 13, 15) 0px -0.5px 2px 0px inset, rgb(13, 13, 15) -0.5px 0px 2px 0px inset",
+            }}
+          >
+            <div className="flex w-full flex-col items-center justify-center text-[5px] text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="h-[6px] w-[6px]"
+              >
+                <path
+                  d="M11.293 7.293a1 1 0 0 1 1.32 -.083l.094 .083l6 6l.083 .094l.054 .077l.054 .096l.017 .036l.027 .067l.032 .108l.01 .053l.01 .06l.004 .057l.002 .059l-.002 .059l-.005 .058l-.009 .06l-.01 .052l-.032 .108l-.027 .067l-.07 .132l-.065 .09l-.073 .081l-.094 .083l-.077 .054l-.096 .054l-.036 .017l-.067 .027l-.108 .032l-.053 .01l-.06 .01l-.057 .004l-.059 .002h-12c-.852 0 -1.297 -.986 -.783 -1.623l.076 -.084l6 -6z"
+                  fill="currentColor"
+                  stroke-width="0"
+                ></path>
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="cursor-pointer rounded-[4px] bg-white/[0.2] p-[0.5px] shadow-md shadow-white/50 transition-transform duration-100 hover:scale-[0.95] hover:shadow-none">
+            <div
+              className="flex h-3 w-6 items-center justify-center rounded-[3.5px] bg-[#0A090D]"
+              style={{
+                boxShadow:
+                  "rgb(13, 13, 15) 0px -0.5px 2px 0px inset, rgb(13, 13, 15) -0.5px 0px 2px 0px inset",
+              }}
+            >
+              <div className="flex w-full flex-col items-center justify-center text-[5px] text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="h-[6px] w-[6px]"
+                >
+                  <path
+                    d="M13.883 5.007l.058 -.005h.118l.058 .005l.06 .009l.052 .01l.108 .032l.067 .027l.132 .07l.09 .065l.081 .073l.083 .094l.054 .077l.054 .096l.017 .036l.027 .067l.032 .108l.01 .053l.01 .06l.004 .057l.002 .059v12c0 .852 -.986 1.297 -1.623 .783l-.084 -.076l-6 -6a1 1 0 0 1 -.083 -1.32l.083 -.094l6 -6l.094 -.083l.077 -.054l.096 -.054l.036 -.017l.067 -.027l.108 -.032l.053 -.01l.06 -.01z"
+                    fill="currentColor"
+                    stroke-width="0"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="cursor-pointer rounded-[4px] bg-white/[0.2] p-[0.5px] shadow-md shadow-white/50 transition-transform duration-100 hover:scale-[0.95] hover:shadow-none">
+            <div
+              className="flex h-3 w-6 items-center justify-center rounded-[3.5px] bg-[#0A090D]"
+              style={{
+                boxShadow:
+                  "rgb(13, 13, 15) 0px -0.5px 2px 0px inset, rgb(13, 13, 15) -0.5px 0px 2px 0px inset",
+              }}
+            >
+              <div className="flex w-full flex-col items-center justify-center text-[5px] text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="h-[6px] w-[6px]"
+                >
+                  <path
+                    d="M18 9c.852 0 1.297 .986 .783 1.623l-.076 .084l-6 6a1 1 0 0 1 -1.32 .083l-.094 -.083l-6 -6l-.083 -.094l-.054 -.077l-.054 -.096l-.017 -.036l-.027 -.067l-.032 -.108l-.01 -.053l-.01 -.06l-.004 -.057v-.118l.005 -.058l.009 -.06l.01 -.052l.032 -.108l.027 -.067l.07 -.132l.065 -.09l.073 -.081l.094 -.083l.077 -.054l.096 -.054l.036 -.017l.067 -.027l.108 -.032l.053 -.01l.06 -.01l.057 -.004l12.059 -.002z"
+                    fill="currentColor"
+                    stroke-width="0"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="cursor-pointer rounded-[4px] bg-white/[0.2] p-[0.5px] shadow-md shadow-white/50 transition-transform duration-100 hover:scale-[0.95] hover:shadow-none">
+            <div
+              className="flex h-3 w-6 items-center justify-center rounded-[3.5px] bg-[#0A090D]"
+              style={{
+                boxShadow:
+                  "rgb(13, 13, 15) 0px -0.5px 2px 0px inset, rgb(13, 13, 15) -0.5px 0px 2px 0px inset",
+              }}
+            >
+              <div className="flex w-full flex-col items-center justify-center text-[5px] text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="h-[6px] w-[6px]"
+                >
+                  <path
+                    d="M9 6c0 -.852 .986 -1.297 1.623 -.783l.084 .076l6 6a1 1 0 0 1 .083 1.32l-.083 .094l-6 6l-.094 .083l-.077 .054l-.096 .054l-.036 .017l-.067 .027l-.108 .032l-.053 .01l-.06 .01l-.057 .004l-.059 .002l-.059 -.002l-.058 -.005l-.06 -.009l-.052 -.01l-.108 -.032l-.067 -.027l-.132 -.07l-.09 -.065l-.081 -.073l-.083 -.094l-.054 -.077l-.054 -.096l-.017 -.036l-.027 -.067l-.032 -.108l-.01 -.053l-.01 -.06l-.004 -.057l-.002 -12.059z"
+                    fill="currentColor"
+                    stroke-width="0"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
